5acb11b1e414073676c7c71c4fcdb80a13305eb6
Suprajanirudi <146161602+Suprajanirudi@users.noreply.github.com>
2023-11-15 12:37:46 +0530

    Add files via upload

---
+++ first-app1.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class FirstComponent extends Component {
+    state = {  } 
+    render() { 
+        return (<h1>From First Component</h1>);
+    }
+}
+ 
+export default FirstComponent;
